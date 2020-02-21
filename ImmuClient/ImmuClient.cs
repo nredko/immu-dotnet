f3675cb779497431277137c5a15ff89a56e6ef01
Nikolay Redko <nredko@nredko.com>
2020-02-21 22:11:40 +0700

    Initial commit.

---
+++ ImmuClient/ImmuClient.cs
@@ -0,0 +1,22 @@
+﻿using Grpc.Net.Client;
+using Immudb.Schema;
+using System;
+
+namespace ImmuClient
+{
+    public class ImmuClient
+    {
+        private readonly ImmuService.ImmuServiceClient instance;
+
+        public ImmuClient(string serverUrl)
+        {
+            var channel = GrpcChannel.ForAddress(serverUrl);
+            instance = new ImmuService.ImmuServiceClient(channel);
+        }
+
+        public ImmuService.ImmuServiceClient GetInstance()
+        {
+            return instance;
+        }
+    }
+}
---
+++ ImmuClient/Utils/Inclusion.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using Google.Protobuf;
+using Immudb.Schema;
+
+namespace ImmuClient.Utils
+{
+    public static class Inclusion
+    {
+        const byte NODE_PREFIX = 1;
+
+        public static bool Verify(InclusionProof inclusionProof, ulong index, ByteString leaf)
+        {
+            if (inclusionProof.Index != index || inclusionProof.Leaf != leaf)
+                return false;
+
+            return VerifyPath(
+                inclusionProof.Path.Select(p => p.ToByteArray()).ToList(),
+                inclusionProof.At,
+                inclusionProof.Index,
+                inclusionProof.Root.ToByteArray(),
+                leaf.ToByteArray()
+            );
+        }
+
+        public static bool VerifyPath(List<byte[]> path, ulong at, ulong i, byte[] root, byte[] leaf)
+        {
+            if (i > at || (at > 0 && path.Count == 0))
+                return false;
+
+            var h = leaf;
+            foreach (var v in path)
+            {
+                var c = new List<byte>();
+                c.Add(NODE_PREFIX);
+
+                if (i % 2 == 0 && i != at)
+                {
+                    c.AddRange(h);
+                    c.AddRange(v);
+                }
+                else
+                {
+                    c.AddRange(v);
+                    c.AddRange(h);
+                }
+                h = SHA256.Create().ComputeHash(c.ToArray());
+                i /= 2;
+                at /= 2;
+            }
+
+            return at == i && h.SequenceEqual(root);
+        }
+    }
+}
